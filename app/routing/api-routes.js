9a5e30e41542f8aa7ad68228cc706b6aabe4c4f0
Brenton Lake <Brenton Lake>
2017-02-08 17:24:19 -0600

    updating node and routes

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,8 +1,8 @@
-var friendsData = require('../app/data/friends.js');
+//var friendsData = require('../app/data/friends.js');
 
-module.exports = function(app){
+//module.exports = function(app){
 
-	app.get('/api/friends', function(req, res){
-		res.json(friendsData);
-	});
-}
+	//app.get('/api/friends', function(req, res){
+		//res.json(friendsData);
+	//});
+//}
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -8,5 +8,7 @@ module.exports = function (app){
 		app.get('/home', function(req, res) {
     res.sendFile(path.join(__dirname + '../public/home.html'));
 });
-
+		app.use(function(req, res) {
+    res.sendFile(path.join(__dirname + '../public/home.html'));
+});
 }
